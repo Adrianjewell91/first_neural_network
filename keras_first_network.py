213e4d824d46036da60691ae8681c9fdd5a53ee6
Adrian <adrianjewell91@gmail.com>
2017-11-16 11:46:52 -0800

    split test data into training and validate

--- keras_first_network.py
+++ keras_first_network.py
@@ -5,24 +5,29 @@ import numpy
 numpy.random.seed(7)
 
 # load pima indians dataset
-dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+dataset = numpy.loadtxt("pima-indians-diabetes_train.csv", delimiter=",")
 # split into input (X) and output (Y) variables
 X = dataset[:,0:8]
 Y = dataset[:,8]
 
+dataset = numpy.loadtxt("pima-indians-diabetes_validate.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X_validate = dataset[:,0:8]
+Y_validate = dataset[:,8]
+
 #create model
 model = Sequential()
 model.add(Dense(12, input_dim=8, activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(10, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # compile model 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #fit the model
-model.fit(X,Y, epochs=10, batch_size=10)
+model.fit(X,Y, epochs=150, batch_size=10)
 
-scores = model.evaluate(X,Y)
+scores = model.evaluate(X_validate,Y_validate)
 print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 
