716b17eecd31ce0d672b64e8e606074d81ee5117
Adrian <adrianjewell91@gmail.com>
2017-11-16 10:53:11 -0800

    successful neural network trained

--- keras_first_network.py
+++ keras_first_network.py
@@ -10,4 +10,19 @@ dataset = numpy.loadtxt("pima-indians-diabetes.csv", delimiter=",")
 X = dataset[:,0:8]
 Y = dataset[:,8]
 
-print(Y)
+#create model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# compile model 
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+#fit the model
+model.fit(X,Y, epochs=10, batch_size=10)
+
+scores = model.evaluate(X,Y)
+print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
+
+
