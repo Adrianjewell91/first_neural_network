ee2e702600aeb260cc8d3e9f3ddb557132c50e5a
Adrian <adrianjewell91@gmail.com>
2017-12-16 22:37:50 -0800

    failed with neocortex, hoping to keras.js before i switch to django

--- node-backend/source/index.js
+++ node-backend/source/index.js
@@ -4,20 +4,22 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
-import NeuralNet from 'neocortex-js';
-let neuralNetInstance = new NeuralNet({
-  modelFilePath: './encoded_model_for_js/model.json',
-  arrayType: 'float64'
-});
+//this doesn't work because I didn't export the weights in a readable way, and I don't know how to load them.
+// import NeuralNet from 'neocortex-js';
+// let neuralNetInstance = new NeuralNet({
+//   modelFilePath: './encoded_model_for_js/model.json',
+//   arrayType: 'float64'
+// });
+
+// neuralNetInstance.init().then (() => {
+//   console.log('yes the neural net worked');
+//   let predictions = neuralNetInstance.predict([0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0]);
+//   res.send('yes it worked on the neural net:', predictions);
+// });
 
 app.use(morgan('combined'));
 
 app.get('/', (req,res) => {
-  neuralNetInstance.init().then (() => {
-    console.log('yes the neural net worked');
-    res.send('yes it worked on the neural net');
-  });
-  // res.send('hello');
 });
 
 app.listen(PORT, () => console.log("listening on port", PORT));
