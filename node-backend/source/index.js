c534271348540f117d3bcbab10bbde9bddd33134
Adrian <adrianjewell91@gmail.com>
2017-12-16 21:54:08 -0800

    backward transpiling works!

--- node-backend/source/index.js
+++ node-backend/source/index.js
@@ -4,20 +4,20 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
-//import NeuralNet from 'neocortex-js';
-// let neuralNetInstance = new NeuralNet({
-//   modelFilePath: './encoded_model_for_js/model.json',
-//   arrayType: 'float64'
-// });
+import NeuralNet from 'neocortex-js';
+let neuralNetInstance = new NeuralNet({
+  modelFilePath: './encoded_model_for_js/model.json',
+  arrayType: 'float64'
+});
 
 app.use(morgan('combined'));
 
 app.get('/', (req,res) => {
-  // neuralNetInstance.init().then (() => {
-  //   console.log('yes the neural net worked');
-  //   res.send('yes it worked on the neural net');
-  // });
-  res.send('hello');
+  neuralNetInstance.init().then (() => {
+    console.log('yes the neural net worked');
+    res.send('yes it worked on the neural net');
+  });
+  // res.send('hello');
 });
 
 app.listen(PORT, () => console.log("listening on port", PORT));
